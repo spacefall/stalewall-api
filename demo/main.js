b68fec35e0d51df75fc402c1e11fe0f5298cc0d4
spacefall <91277246+spacefall@users.noreply.github.com>
2023-07-10 17:43:55 +0200

    Added a Chromecast Homepage-like demo

---
+++ demo/main.js
@@ -0,0 +1,48 @@
+const cssvars = document.querySelector(":root");
+const timeBetweenImagesInSecs = 1000;
+let nextwallurl = "";
+let visibleBg = 1; //is set to 1 to have a nice transition when first loading the page
+
+function newWall() {
+    console.log("wall");
+    fetch("https://stalewall.vercel.app/api?bQlt=80")
+        .then((res) => res.json())
+        .then((out) => preloadAndSet(out["url"]))
+        .catch((err) => {
+            throw err;
+        });
+}
+
+function preloadAndSet(url) {
+    cssvars.style.setProperty("--bg-preload", "url(" + url + ")");
+    if (visibleBg == 0) {
+        cssvars.style.setProperty("--bg-img-1", "url(" + nextwallurl + ")");
+        cssvars.style.setProperty("--bg-1-opacity", "1  ");
+        visibleBg = 1;
+    } else {
+        cssvars.style.setProperty("--bg-img-0", "url(" + nextwallurl + ")");
+        cssvars.style.setProperty("--bg-1-opacity", "0");
+        visibleBg = 0;
+    }
+    nextwallurl = url;
+}
+
+// Gets time every second and sets the appropriate divs
+function tick() {
+    const now = dayjs();
+    document.getElementById("clock").innerHTML = now.format("HH:mm");
+    document.getElementById("date").innerHTML = now.format("dddd, DD MMMM");
+}
+
+function setupAndRun() {
+    if (navigator.language === "it-IT") {
+        dayjs.locale("it");
+    }
+    newWall();
+    tick();
+    newWall();
+    setInterval(tick, 1000);
+    setInterval(newWall, timeBetweenImagesInSecs * 1000);
+}
+
+document.addEventListener("DOMContentLoaded", setupAndRun);
